b31b41d070374a35d49e92fa5fdf192f89f3911c
Andriy <grushevoyandrey@gmail.com>
2025-05-29 16:14:07 +0100

    Add user registration, login, and profile management views

--- user/views.py
+++ user/views.py
@@ -1,21 +1,22 @@
-from rest_framework import generics
+from user.serializers import AuthTokenSerializer
+from rest_framework import generics, serializers
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-
 from user.serializers import UserSerializer
 
 
-class CreateUserView(generics.CreateAPIView):
+class CreateUserView(generics.CreateAPIView):  # register/
     serializer_class = UserSerializer
 
 
-class CreateTokenView(ObtainAuthToken):
+class CreateTokenView(ObtainAuthToken):  # login/
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
 
 
-class ManageUserView(generics.RetrieveUpdateAPIView):
+class ManageUserView(generics.RetrieveUpdateAPIView):  # me/
     serializer_class = UserSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
